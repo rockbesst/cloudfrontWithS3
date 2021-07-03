37752e9dfd48f52bb3f287422c8e3bab357137cd
Rockbesst Win <rockbesst@gmail.com>
2021-07-03 08:11:42 +0300

    1st version

---
+++ main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+	region = var.region
+}
+
+locals {
+    origin_id = "S3"
+}
+
+resource "aws_cloudfront_distribution" "main_dist" {
+    origin {
+    #     custom_origin_config {
+    #         http_port              = 80
+    #         https_port             = 443
+    #         origin_protocol_policy = "http-only"
+    #         origin_ssl_protocols   = ["TLSv1.2"]
+    # }
+        domain_name = aws_s3_bucket.b.bucket_domain_name
+        origin_id = local.origin_id
+    }
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = local.origin_id
+        viewer_protocol_policy = "allow-all"
+        forwarded_values{
+            query_string = false
+            cookies {
+                forward = "none"
+            }
+        }
+    }
+    enabled = true
+    restrictions {
+        geo_restriction {
+            restriction_type = "whitelist"
+            locations = ["UA", "PL"]
+        }
+    }
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+
+resource "aws_s3_bucket" "b" {
+    acl    = "public-read"
+    website {
+        index_document = "index.html"
+            }
+}
+
+resource "aws_s3_bucket_object" "index" {
+  key        = "index.html"
+  bucket     = aws_s3_bucket.b.id
+  source     = "/files/site/index.html"
+}
+resource "aws_s3_bucket_object" "image" {
+  key        = "IMG_3993.jpg"
+  bucket     = aws_s3_bucket.b.id
+  source     = "/files/site/IMG_3993.jpg"
+}
+
+}
---
+++ sec.tf
@@ -0,0 +1,3 @@
+data "aws_security_group" "mainSecGroup" {
+ id = "sg-0ef96a12de408930c"
+}
