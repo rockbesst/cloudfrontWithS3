e549e9517009336b6ec2ae97f3f4e49bb854a2c8
Rockbesst Win <rockbesst@gmail.com>
2021-07-03 08:33:52 +0300

    fix acl

--- s3.tf
+++ s3.tf
@@ -1,4 +1,5 @@
 resource "aws_s3_bucket" "b" {
+    bucket = "tf-s3-rockbesst"
     acl    = "public-read"
     website {
         index_document = "index.html"
@@ -7,11 +8,13 @@ resource "aws_s3_bucket" "b" {
 
 resource "aws_s3_bucket_object" "index" {
   key        = "index.html"
+  acl    = "public-read"
   bucket     = aws_s3_bucket.b.id
   source     = "./files/site/index.html"
 }
 resource "aws_s3_bucket_object" "image" {
   key        = "IMG_3993.jpg"
+  acl    = "public-read"
   bucket     = aws_s3_bucket.b.id
   source     = "./files/site/IMG_3993.jpg"
 }
