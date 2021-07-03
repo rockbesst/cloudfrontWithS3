896191827318d0c5f86542f396428628878321df
Rockbesst Win <rockbesst@gmail.com>
2021-07-03 08:26:38 +0300

    fix

--- s3.tf
+++ s3.tf
@@ -8,10 +8,10 @@ resource "aws_s3_bucket" "b" {
 resource "aws_s3_bucket_object" "index" {
   key        = "index.html"
   bucket     = aws_s3_bucket.b.id
-  source     = "/files/site/index.html"
+  source     = "./files/site/index.html"
 }
 resource "aws_s3_bucket_object" "image" {
   key        = "IMG_3993.jpg"
   bucket     = aws_s3_bucket.b.id
-  source     = "/files/site/IMG_3993.jpg"
+  source     = "./files/site/IMG_3993.jpg"
 }
